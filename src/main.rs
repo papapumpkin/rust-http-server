d715fe2acfa51a3463287968b63bf72794d9cb2f
asalm <asalm@octavebio.com>
2023-12-28 15:03:28 -0500

    not pass 2nd stage

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,29 @@
 use std::net::TcpListener;
 
-fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
+fn handle_connection(stream: TcpStream) {
+    match stream.accept() {
+        Ok((_socket, addr)) => println!("new client: {addr:?}"),
+        Err(e) => println!("couldn't get client: {e:?}"),
+    }
+ }
+
+fn main() -> std::io::Result<()> {
+    println!("Starting server...");
 
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
+
+    println!("Server listening on port 4221...");
 
     for stream in listener.incoming() {
         match stream {
             Ok(_stream) => {
                 println!("accepted new connection");
+                handle_connection(stream)
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
     }
+    Ok(())
 }
