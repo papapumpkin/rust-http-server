f31b0d7f9a27e7a994a9324b8dddc71f73a9f438
asalm <asalm@octavebio.com>
2023-12-29 18:05:15 -0500

    pass 7th stage

--- src/file.rs
+++ src/file.rs
@@ -1,6 +1,5 @@
 use std::fs;
 use std::path::Path;
-use std::io::{self, ErrorKind};
 
 pub fn parse_filename_from_request_path(path: &str) -> Option<String> {
     let filename = path[7..].to_string(); // Assuming '/files/' is always present
@@ -17,15 +16,9 @@ pub fn parse_filename_from_request_path(path: &str) -> Option<String> {
     path.to_str().map(|s| s.to_string())
 }
 
-pub fn read_file_to_string(file_path: &Path) -> Result<String, std::io::Error> {
+pub fn read_file_to_string(file_path: &Path) -> Option<String> {
     match fs::read_to_string(file_path) {
-        Ok(content) => Ok(content),
-        Err(e) => {
-            match e.kind() {
-                ErrorKind::NotFound => Err(io::Error::new(ErrorKind::NotFound, format!("File not found: {}", file_path.display()))),
-                ErrorKind::PermissionDenied => Err(io::Error::new(ErrorKind::PermissionDenied, "Permission denied")),
-                _ => Err(e),
-            }
-        }
+        Ok(content) => Some(content),
+        Err(_) => None
     }
 }
