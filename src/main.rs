732a844e9df55aa40897103b59d05097efcee233
asalm <asalm@octavebio.com>
2023-12-30 12:29:44 -0500

    pass 8th chamber

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::io::{self, Read, Write};
+use std::io::{self, Write};
 use std::net::{TcpListener, TcpStream};
 use std::path::Path;
 use std::sync::Arc;
@@ -15,88 +15,80 @@ use config::Config;
 use http::{HTTPBody, HTTPContentType, HTTPStatus};
 use response::HTTPResponse;
 
-fn handle_connection(mut stream: TcpStream, config: Arc<Config>) -> io::Result<()> {
+fn handle_connection(mut stream: TcpStream, _config: Arc<Config>) -> io::Result<()> {
     println!("Accepted new connection!");
-    let mut buffer = vec![0u8; config.buffer_size];
 
-    match stream.read(&mut buffer) {
-        Ok(_) => {
-            let response = if let Some(request) = request::parse_request(&buffer) {
-                match request.path.as_str() {
-                    "/" => HTTPResponse {
-                        status: HTTPStatus::Ok,
-                        body: None,
-                    },
-                    "/user-agent" => HTTPResponse {
+    match request::parse_stream(&stream) {
+        Ok(request) => {
+            let response = match request.headers.path.as_str() {
+                "/" => HTTPResponse {
+                    status: HTTPStatus::Ok,
+                    body: None,
+                },
+                "/user-agent" => HTTPResponse {
+                    status: HTTPStatus::Ok,
+                    body: Some(HTTPBody {
+                        body: request.headers.user_agent,
+                        content_type: HTTPContentType::PlainText,
+                    }),
+                },
+                path if path.starts_with("/echo/") => {
+                    let to_echo = &path[6..];
+                    let to_echo = to_echo.to_string();
+                    HTTPResponse {
                         status: HTTPStatus::Ok,
                         body: Some(HTTPBody {
-                            body: request.user_agent,
+                            body: to_echo,
                             content_type: HTTPContentType::PlainText,
                         }),
-                    },
-                    path if path.starts_with("/echo/") => {
-                        let to_echo = &path[6..];
-                        let to_echo = to_echo.to_string();
-                        HTTPResponse {
-                            status: HTTPStatus::Ok,
-                            body: Some(HTTPBody {
-                                body: to_echo,
-                                content_type: HTTPContentType::PlainText,
-                            }),
-                        }
                     }
-                    path if path.starts_with("/files/") => {
-                        let directory =
-                            cli::get_cli_arg_by_name("--directory").expect("Argument not found");
+                }
+                path if path.starts_with("/files/") => {
+                    let directory =
+                        cli::get_cli_arg_by_name("--directory").expect("Argument not found");
 
-                        let safe_filename = file::parse_filename_from_request_path(&path)
-                            .expect("Invalid filename in request");
+                    let safe_filename = file::parse_filename_from_request_path(&path)
+                        .expect("Invalid filename in request");
 
-                        let full_path = Path::new(&directory).join(safe_filename);
-                        println!("Full path to file: {}", full_path.display());
-                        if request.method == "GET" {
-                            let result = match file::read_file_to_string(&full_path) {
-                                Some(file_content) => HTTPResponse {
-                                    status: HTTPStatus::Ok,
-                                    body: Some(HTTPBody {
-                                        body: file_content,
-                                        content_type: HTTPContentType::File,
-                                    }),
-                                },
-                                None => HTTPResponse {
-                                    status: HTTPStatus::NotFound,
-                                    body: None,
-                                },
-                            };
-                            result
-                        } else if request.method == "POST" {
-                            let body = request.body.unwrap();
-                            file::write_string_to_file(&full_path, &body)?;
-
-                            HTTPResponse {
-                                status: HTTPStatus::Created,
+                    let full_path = Path::new(&directory).join(safe_filename);
+                    println!("Full path to file: {}", full_path.display());
+                    if request.headers.method == "GET" {
+                        let result = match file::read_file_to_string(&full_path) {
+                            Some(file_content) => HTTPResponse {
+                                status: HTTPStatus::Ok,
                                 body: Some(HTTPBody {
-                                    body: body,
+                                    body: file_content,
                                     content_type: HTTPContentType::File,
                                 }),
-                            }
-                        } else {
-                            HTTPResponse {
-                                status: HTTPStatus::BadRequest,
+                            },
+                            None => HTTPResponse {
+                                status: HTTPStatus::NotFound,
                                 body: None,
-                            }
+                            },
+                        };
+                        result
+                    } else if request.headers.method == "POST" {
+                        let body = request.body.unwrap();
+                        file::write_string_to_file(&full_path, &body)?;
+
+                        HTTPResponse {
+                            status: HTTPStatus::Created,
+                            body: Some(HTTPBody {
+                                body: body,
+                                content_type: HTTPContentType::File,
+                            }),
+                        }
+                    } else {
+                        HTTPResponse {
+                            status: HTTPStatus::BadRequest,
+                            body: None,
                         }
                     }
-                    _ => HTTPResponse {
-                        status: HTTPStatus::NotFound,
-                        body: None,
-                    },
                 }
-            } else {
-                HTTPResponse {
-                    status: HTTPStatus::InternalServerError,
+                _ => HTTPResponse {
+                    status: HTTPStatus::NotFound,
                     body: None,
-                }
+                },
             };
             println!("{}", format!("{}", response));
             stream.write_all(response.to_string().as_bytes())?;
--- src/request.rs
+++ src/request.rs
@@ -1,68 +1,90 @@
-pub struct ParsedRequest {
+use std::io::{self, BufReader, BufRead, Read};
+use std::net::TcpStream;
+
+pub struct RequestHeaders {
     pub method: String,
     pub path: String,
     pub user_agent: String,
     pub content_length: Option<usize>,
+}
+
+pub struct ParsedRequest {
+    pub headers: RequestHeaders,
     pub body: Option<String>,
 }
 
-fn get_request_path(request_line: &str) -> Option<String> {
+fn get_request_path(request_line: &str) -> String {
     request_line
         .split_whitespace()
         .nth(1)
-        .map(|s| s.to_string())
+        .map(|s| s.to_string()).unwrap()
 }
 
-fn get_user_agent(request_line: &str) -> Option<String> {
+fn get_user_agent(request_line: &str) -> String {
     request_line
         .split_whitespace()
         .nth(1)
-        .map(|s| s.to_string())
+        .map(|s| s.to_string()).unwrap()
 }
 
-fn get_request_method(request_line: &str) -> Option<String> {
+fn get_request_method(request_line: &str) -> String {
     request_line
         .split_whitespace()
         .nth(0)
-        .map(|s| s.to_string())
+        .map(|s| s.to_string()).unwrap()
 }
 
-fn get_content_length(request_line: &str) -> Option<usize> {
+fn get_content_length(request_line: &str) -> usize {
     let parts: Vec<&str> = request_line.split_whitespace().collect();
-    Some(parts[1].parse::<usize>().unwrap_or(0))
+    parts[1].parse::<usize>().unwrap_or(0)
 }
 
-// fn get_request_body(request_line: &str) -> Option<usize> {
-
-// }
-
-pub fn parse_request(buffer: &[u8]) -> Option<ParsedRequest> {
-    let request_str = String::from_utf8_lossy(buffer);
-    let request_lines: Vec<&str> = request_str.split_terminator("\r\n").collect();
-
-    let method = get_request_method(&request_lines[0])?;
-    let path = get_request_path(&request_lines[0])?;
-    let user_agent = get_user_agent(&request_lines[2])?;
+pub fn parse_request_headers(headers: &str) -> RequestHeaders {
+    let header_lines: Vec<&str> = headers.split("\r\n").collect();
+    let method = get_request_method(header_lines[0]);
+    let path = get_request_path(header_lines[0]);
+    let user_agent = get_user_agent(header_lines[2]);
 
     let content_length = if method == "POST" {
-        Some(get_content_length(&request_lines[4])?)
+        Some(get_content_length(header_lines[4]))
     } else {
         None
     };
 
-    let body = if method == "POST" {
-        Some(request_lines[7].to_string())
-    } else {
-        None
-    };
-
-    let parsed_request = ParsedRequest {
+    RequestHeaders {
         method,
         path,
         user_agent,
         content_length,
-        body,
-    };
+    }
+}
 
-    Some(parsed_request)
+pub fn parse_stream(stream: &TcpStream) -> io::Result<ParsedRequest> {
+    let mut reader = BufReader::new(stream);
+    let mut headers = String::new();
+
+    // Read headers
+    loop {
+        let mut line = String::new();
+        reader.read_line(&mut line)?;
+        if line == "\r\n" {
+            break; // End of headers
+        }
+        headers.push_str(&line);
+    }
+
+    let parsed_headers = parse_request_headers(&headers);
+
+    // Read the body
+    let mut body_bytes = vec![0; parsed_headers.content_length.unwrap_or(0)];
+    reader.read_exact(&mut body_bytes)?;
+
+    // Convert body to String
+    let body_str = String::from_utf8(body_bytes)
+        .map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e))?;
+
+    Ok(ParsedRequest {
+        headers: parsed_headers,
+        body: Some(body_str),
+    })
 }
